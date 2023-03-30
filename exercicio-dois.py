1d4a97149f3bb6769de08f8ed09ebab40e783aa1
unknown <natalia.archanjo@gmail.com>
2023-03-30 16:47:57 -0300

    foi criada uma lista de numeros que aparec com os numeros invertidos com auxilio da repetiçao

---
+++ exercicio-dois.py
@@ -0,0 +1,6 @@
+listaNumeros = ['1','2','3','4','5','6','7','8','9','10']
+listavazia=[]
+
+for i in range (0, 10):
+    listavazia.append(listaNumeros.pop())
+print(listavazia)
---
+++ exercico-um.py
@@ -0,0 +1,3 @@
+listaDeNumeros = ['1','2','3','4','5']
+
+print(listaDeNumeros)
