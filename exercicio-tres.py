76e55b82c83de16a191a240269519bec5a28ba6d
unknown <natalia.archanjo@gmail.com>
2023-04-06 11:21:31 -0300

    foi realizado um progama que calcula a media dos aulunos

---
+++ exercicio-tres.py
@@ -0,0 +1,11 @@
+notas = [70, 80, 90, 100]
+print('notas do aluno:' , notas)
+
+quantidade=4
+soma=0
+
+for valores in notas:
+    soma=soma+valores
+
+print('media total do aluno:', soma/quantidade)
+
