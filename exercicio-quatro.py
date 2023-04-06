40daca17813653195a60051f00094a442d1bdf56
unknown <natalia.archanjo@gmail.com>
2023-04-06 14:51:28 -0300

    foi feito um progama que mostra quais são as consoantes e o número de consoante de uma palavra

---
+++ exercicio-quatro.py
@@ -0,0 +1,13 @@
+palavra = ['h','i','p','o','c','r','i','s','i','a']
+vogal = ['a', 'e', 'i', 'o', 'u']
+numerodeconsoantes = 0
+print('palavra =',palavra)
+
+for letras in palavra:
+    if letras not in vogal:
+        numerodeconsoantes = numerodeconsoantes + 1
+        print('consoantes :',letras)
+print('número de consoantes =', numerodeconsoantes)
+
+
+
