28df88946115e9f47e48b77ef8639517ab771cbd
unknown <natalia.archanjo@gmail.com>
2023-04-14 10:21:46 -0300

    foi feito um progama que pede uma nota entre 0 e 10 e se não tiver entre esses números aparece uma mensagem de erro e precisa digitar ate esta valido

--- README.md
+++
@@ -1,2 +0,0 @@
-# exercicios-lista-e-repeticao
-Exercícios de produção de lista e treinar repetição
--- exercicio-02.py
+++
@@ -1,6 +0,0 @@
-listaNumeros = ['1','2','3','4','5','6','7','8','9','10']
-listavazia=[]
-
-for i in range (0, 10):
-    listavazia.append(listaNumeros.pop())
-print(listavazia)
