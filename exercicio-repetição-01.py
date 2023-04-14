2200350703cc9ad4468ff8ec3cad312915f3c0f2
unknown <natalia.archanjo@gmail.com>
2023-04-14 09:53:43 -0300

    foi feito um progama que pede uma nota entre 0 e 10 e se não tiver entre esses números aparece uma mensagem de erro e precisa digitar ate esta valido

---
+++ exercicio-repetição-01.py
@@ -0,0 +1,11 @@
+
+while True:
+    nota = float(input('Digite uma nota entre 0 e 10:'))
+    if nota >= 0 and nota <= 10:
+        print('nota está valida obrigada(o), nota inserida:', nota)
+        break
+    else:
+        print('Nota invalida, insira uma nota entre 0 ou 10')
+
+
+
